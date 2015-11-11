072a3fe4673901ac03810a0fb4c5d8864b22ffbd
ben garside <ben.garside@bsigroup.com>
2015-11-11 15:21:29 +0000

    xes prod send not working

--- src/php/dbinit.prd.php
+++ src/php/dbinit.prd.php
@@ -1,6 +1,6 @@
 <?php
 $GLOBALS['config']['mysql'] = array(
-
+	
 		// UAT DATABASE - DEVELOPMENT	
 		'host' => '10.103.109.84\cloud,1500',
 		'username' => 'XCP_user',
@@ -10,25 +10,25 @@ $GLOBALS['config']['mysql'] = array(
 	);
 
 $GLOBALS['config']['sendItems']['global'] = array(
-		'subMain' 		=> "Innodata_Batch_Alert_",
+		'globalSubject' => "Innodata_Batch_Alert_",
 		'globalBody'	=> "<p>Dear Innodata,</p><p>This is to inform you that a new batch has been transferred to Innodata\'s FTP site, for review for producing agreed XML outputs. Attached is a transmission sheet detailing each Material (identified by UPI) in the batch.</p><p><strong>Please could you confirm receipt of these files and get back on an estimated turnaround time (TAT) and cost. Following this we will confirm whether work should proceed.</strong></p><p><u>Notes:</u></p><p>Directory path: /TO INNO/</p><ul><li>Materials are batched into zip files according to a single Conversion Pipeline (indicated in the Batch ID and the column Conversion_Pipeline in the transmission sheet)</li><li>Each Material in the zip file needs to be processed through that Conversion Pipeline </li><li>Details in the transmission sheet confirm the file types we have provided for each Material (see columns Source_File_Type) and the required XML output (based on Schema, see column Output_File_Type)</li></ul><p>Many thanks</p>",
 		'systemUser'	=> -1,
-		'recNote'		=> array("TO" => "ben.garside@bsigroup.com")
+		'recNote'		=> array("TO" => "ben.garside@bsigroup.com", "CC" => "ben.garside@bsigroup.com")
 	);
 
 $GLOBALS['config']['sendItems']['dev'] = array(
 
 		//set email settings for main send 
-		'subMain' => "UAT::PRE_SEND::" . $GLOBALS['config']['sendItems']['global']['globalSubject'],
+		'subMain' => "PRE-SEND:" . $GLOBALS['config']['sendItems']['global']['globalSubject'],
 		'bodMain' => $GLOBALS['config']['sendItems']['global']['globalBody'],
 		'recMain' => array(	"CC" => "ben.garside@bsigroup.com",
-							"TO" => "content.operations@bsigroup.com"),
+							"TO" => "Varun.Verma@bsigroup.com"),
 
 		//Email settings for Pipeline 8 send 
-		'subPl8' => "UAT::PRE_SEND(PL8)::" . $GLOBALS['config']['sendItems']['global']['globalSubject'],
+		'subPl8' => "PRE-SEND[PL8]:" . $GLOBALS['config']['sendItems']['global']['globalSubject'],
 		'bodPl8' => $GLOBALS['config']['sendItems']['global']['globalBody'],
-		'recPl8' => array(	"CC" => "ben.garside@bsigroup.com",
-							"TO" => "content.operations@bsigroup.com"),
+		'recPl8' => array(	"CC" => "Varun.Verma@bsigroup.com",
+							"TO" => "ben.garside@bsigroup.com"),
 
 		// FTP settings
 		'ftp_server' 	=> "ftp.hugatramp.com",
@@ -49,21 +49,30 @@ $GLOBALS['config']['sendItems']['dev'] = array(
 $GLOBALS['config']['sendItems']['prd'] = array(
 
 		//set email settings for main send 
-		'subMain' => "UAT::" . $GLOBALS['config']['sendItems']['global']['globalSubject'],
+		'subMain' => $GLOBALS['config']['sendItems']['global']['globalSubject'],
 		'bodMain' => $GLOBALS['config']['sendItems']['global']['globalBody'],
-		'recMain' => array(	"CC" => "ben.garside@bsigroup.com",
-							"TO" => "content.operations@bsigroup.com"),
+		'recMain' => array(	"BCC" => "ben.garside@bsigroup.com",
+							"CC" => "content.operations@bsigroup.com",
+							"TO" => "LGoboy@INNODATA.COM",
+							"TO" => "APradeep@innodata.com",
+							"TO" => "PPerera@INNODATA.COM",
+							"TO" => "KJayanetti@innodata.com",
+							"TO" => "SPerera@INNODATA.COM",
+							"TO" => "Customer-Service@INNODATA.COM"),						
 
 		//Email settings for Pipeline 8 send 
-		'subPl8' => "UAT(PL8)::" . $GLOBALS['config']['sendItems']['global']['globalSubject'],
+		'subPl8' => $GLOBALS['config']['sendItems']['global']['globalSubject'],
 		'bodPl8' => $GLOBALS['config']['sendItems']['global']['globalBody'],
-		'recPl8' => array(	"CC" => "ben.garside@bsigroup.com",
-							"TO" => "content.operations@bsigroup.com"),
+		'recPl8' => array(	"BCC" => "ben.garside@bsigroup.com",
+							"CC" => "content.operations@bsigroup.com",
+							"TO" => "gcmontesclaros@INNODATA.COM",
+							"TO" => "MJaucian@innodata.com",
+							"TO" => "ARQuismundo@innodata.com"),
 
 		// FTP settings
-		'ftp_server' 	=> "ftp.hugatramp.com",
-		'ftp_user_name' => "xcptest1@hugatramp.com",
-		'ftp_user_pass' => "Password1",
+		'ftp_server' 	=> "ftpnda.innodata.com",
+		'ftp_user_name' => "FTP-BSI",
+		'ftp_user_pass' => "PL4789mn",
 
 		// What items to check for and where to move them
 		'activityFrom' 	=> '10',
